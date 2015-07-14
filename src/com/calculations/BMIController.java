312d9edb99781dc2ad9ee8cb2ce415ab10c5fe0a
harirehm <harirehm@gmail.com>
2015-07-14 15:42:48 +0530

    sending email

--- src/com/calculations/BMIController.java
+++ src/com/calculations/BMIController.java
@@ -1,12 +1,23 @@
 package com.calculations;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.*;
 
 import com.valuepack.ValueProvider;
+
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @Controller
 public class BMIController 
@@ -112,9 +123,26 @@ public class BMIController
 	}
 	
 	@RequestMapping("/MailSend")
-	public ModelAndView sendMail(@RequestParam(value="name") String name,@RequestParam(value="email") String email,@RequestParam(value="msg") String msg)
+	public ModelAndView sendMail(@RequestParam(value="name") String userName,@RequestParam(value="email") String userEmail,@RequestParam(value="msg") String msgBody)
 	{
 		ModelAndView model=new ModelAndView("MailSent");
+		Properties prop = new Properties();
+	    Session session = Session.getDefaultInstance(prop,null);
+	    try{    
+	        Message mimeMessage = new MimeMessage(session);
+	        mimeMessage.setFrom(new InternetAddress("harirehm@gmail.com"));
+	        mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress("harirehm@gmail.com", "Mr./Ms. "+userName));
+	        mimeMessage.setSubject("Message from BMI calculations app");
+	        mimeMessage.setText("Name :: "+userName+"\nEmail-id :: "+userEmail+"\n\nMessage body ::\n"+msgBody);
+	        Transport.send(mimeMessage);
+	        System.out.println("Successfull Delivery.");
+	    } catch (AddressException e) {
+	        e.printStackTrace();
+	    } catch (MessagingException e) {
+	        e.printStackTrace();
+	    } catch (UnsupportedEncodingException e) {
+	        e.printStackTrace();
+	    }
 		return model;
 	}
 	
